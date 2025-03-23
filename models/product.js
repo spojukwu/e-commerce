aa10776dea2659d72d097cbc7f3801c5aafd9b3c
Stanley Ifeanyi <ifeanyistanley82@gmail.com>
2025-03-23 04:47:50 +0100

    update

--- models/product.js
+++ models/product.js
@@ -8,7 +8,7 @@ const productSchema = new mongoose.Schema({
     stock: { type: Number, required: true, min: 0 }, // Stock level   
     images: [{ type: String }], // Array of image URLs  
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },  
-    //inventory: { type: Number, default: 0, min: 0 },  
+    inventory: { type: Number, default: 0, min: 0 },  
     createdAt: { type: Date, default: Date.now },  
     updatedAt: { type: Date, default: Date.now }  
 });  
